1963c14b4e304c0f1b98cc5440470df8143c1ab8
Roshan Paudel <rozanpoudel@gmail.com>
2023-06-15 22:35:59 +0545

    Backed up!!

--- backup.sh
+++ backup.sh
@@ -6,6 +6,17 @@ PRESENT_WORKING_DIR=$(pwd)
 
 cd $BACKUP_DIR
 
+
+
+TMUX_DIR="$BACKUP_DIR/tmux"
+
+if [ ! -d "$TMUX_DIR" ]; then
+  mkdir "$TMUX_DIR"
+  echo "Directory created: $TMUX_DIR"
+else
+  echo "Directory already exists: $TMUX_DIR"
+fi
+
 cp -r ~/.config/fish/ $BACKUP_DIR
 mkdir $BACKUP_DIR/tmux
 cp -r ~/.config/tmux/tmux.conf $BACKUP_DIR/tmux
