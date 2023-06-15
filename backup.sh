a626b68cee29e6ebb18197592bcca24a952847bb
Roshan Paudel <rozanpoudel@gmail.com>
2023-06-15 22:33:36 +0545

    Backed up!!

--- backup.sh
+++ backup.sh
@@ -2,6 +2,10 @@
 
 BACKUP_DIR=~/Public/dotfiles/
 
+PRESENT_WORKING_DIR=$(pwd)
+
+cd $BACKUP_DIR
+
 cp -r ~/.config/fish/ $BACKUP_DIR
 mkdir $BACKUP_DIR/tmux
 cp -r ~/.config/tmux/tmux.conf $BACKUP_DIR/tmux
@@ -11,4 +15,6 @@ cp -r ~/.config/vlc $BACKUP_DIR
 git add $BACKUP_DIR
 git commit -m "Backed up!!"
 
+cd $PRESENT_WORKING_DIR
 echo Done !!!
+
