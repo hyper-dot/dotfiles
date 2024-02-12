22d725b28ceaef49519e41ed1527cf5b9e9b2e8c
Roshan Paudel <rozanpoudel@gmail.com>
2024-02-12 22:51:52 +0545

    AUTO COMMIT 2024-02-12 22:51:52

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,61 +1,61 @@
 local plugins = {
-  {
-    "nvimtools/none-ls.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return require "custom.configs.null-ls"
-    end,
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    ft = {
-      "javascript",
-      "javascriptreact",
-      "typescript",
-      "typescriptreact",
-    },
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "typescript-language-server",
-        "tailwindcss-language-server",
-        "prettierd",
-        "emmet-ls",
-        "stylua",
-        "css-lsp",
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function()
-      local opts = require "plugins.configs.treesitter"
-      opts.ensure_installed = {
-        "lua",
-        "javascript",
-        "typescript",
-        "tsx",
-      }
-      return opts
-    end,
-  },
+	{
+		"nvimtools/none-ls.nvim",
+		event = "VeryLazy",
+		opts = function()
+			return require("custom.configs.null-ls")
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		ft = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+		},
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"typescript-language-server",
+				"tailwindcss-language-server",
+				"prettierd",
+				"emmet-ls",
+				"stylua",
+				"css-lsp",
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function()
+			local opts = require("plugins.configs.treesitter")
+			opts.ensure_installed = {
+				"lua",
+				"javascript",
+				"typescript",
+				"tsx",
+			}
+			return opts
+		end,
+	},
 
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
@@ -64,18 +64,30 @@ local plugins = {
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
-  },
-  { "folke/which-key.nvim", enabled = false },
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
-  },
-  { "wakatime/vim-wakatime", lazy = false },
+	},
+	{ "folke/which-key.nvim", enabled = false },
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{ "wakatime/vim-wakatime", lazy = false },
+	{
+		"HakonHarnes/img-clip.nvim",
+		event = "BufEnter",
+		opts = {
+			-- add options here
+			-- or leave it empty to use the default settings
+		},
+		keys = {
+			-- suggested keymap
+			{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+		},
+	},
 }
 return plugins
