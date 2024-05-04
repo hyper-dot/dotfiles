c0a872e57baee22d6fdf0263723e674741f03fd0
Roshan Paudel <rozanpoudel@gmail.com>
2024-05-04 10:14:35 +0545

    ✨ NEW: added neovim configs

--- .config/nvim/lua/custom/init.lua
+++
@@ -1,5 +0,0 @@
-vim.opt.backup = false
-vim.opt.swapfile = false
-vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
-vim.opt.iskeyword:append("-")
-vim.opt.signcolumn = "yes" -- show sign column so that text doesn't shift
---
+++ mac.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd .config
+stow -t ~/.config alacritty
