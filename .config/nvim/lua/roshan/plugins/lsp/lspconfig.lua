8f3b2366f24f178e4e3f6692a772c6d68ba2671d
Roshan Paudel <rozanpoudel@gmail.com>
2024-07-30 15:39:49 +0545

    👌 IMPROVE: removed lspsaga

--- .config/nvim/lua/roshan/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/roshan/plugins/lsp/lspconfig.lua
@@ -17,20 +17,18 @@ return {
       callback = function(ev)
         local opts = { buffer = ev.buf, silent = true }
         -- set keybinds
-        k.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)                  -- show definition, references
+        k.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)            -- show definition, references
         -- k.set("n", "gD", "<cmd>Telescope lsp_decleration<CR>", opts) -- go to declaration
-        k.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)           -- see definition and make edits in window
-        k.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)       -- go to implementation
-        k.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)         -- see available code actions
-        k.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)              -- smart rename
-        k.set("n", "<leader>D", "<cmd>Lspsaga show_buf_diagnostics<CR>", opts) -- show diagnostics for buffer
-        -- k.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show diagnostics for cursor
-        k.set("n", "<leader>d", vim.diagnostic.open_float, opts)               -- show diagnostics for cursor
-        k.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)        -- jump to previous diagnostic in buffer
-        k.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)        -- jump to next diagnostic in buffer
-        k.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)                    -- show documentation for what is under cursor
-        -- k.set("n", "K", vim.lsp.buf.hover, opts)                               -- show documentation for what is under cursor
-        k.set("n", "<leader>rn", vim.lsp.buf.rename, opts)                     -- smart rename
+        k.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)     -- see definition and make edits in window
+        k.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- go to implementation
+        -- k.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)         -- see available code actions
+        k.set("n", "<leader>D", "<cmd>Telescope diagnostics<CR>", opts)  -- show diagnostics for buffer
+        k.set("n", "<leader>d", vim.diagnostic.open_float, opts)         -- show diagnostics for cursor
+        k.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)  -- jump to previous diagnostic in buffer
+        k.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)  -- jump to next diagnostic in buffer
+        -- k.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)                    -- show documentation for what is under cursor
+        k.set("n", "K", vim.lsp.buf.hover, opts)                         -- show documentation for what is under cursor
+        k.set("n", "<leader>rn", vim.lsp.buf.rename, opts)               -- smart rename
       end
     })
 
@@ -41,6 +39,10 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
     end
 
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+    })
 
 
     vim.diagnostic.config({
--- .config/nvim/lua/roshan/plugins/telescope.lua
+++ .config/nvim/lua/roshan/plugins/telescope.lua
@@ -10,21 +10,89 @@ return {
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        -- path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous,                       -- move to prev result
-            ["<C-j>"] = actions.move_selection_next,                           -- move to next result
-            ["<S-Tab>"] = actions.move_selection_previous,                     -- move to prev result
-            ["<Tab>"] = actions.move_selection_next,                           -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-          }
-        }
-      }
-    })
+    -- telescope.setup({
+    --   defaults = {
+    --     path_display = { "smart" },
+    --     mappings = {
+    --       i = {
+    --         ["<C-k>"] = actions.move_selection_previous,                       -- move to prev result
+    --         ["<C-j>"] = actions.move_selection_next,                           -- move to next result
+    --         ["<S-Tab>"] = actions.move_selection_previous,                     -- move to prev result
+    --         ["<Tab>"] = actions.move_selection_next,                           -- move to next result
+    --         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+    --       }
+    --     }
+    --   }
+    -- })
 
+    telescope.setup(
+      {
+        pickers = {
+          find_files = {
+            hidden = true
+          }
+        },
+        defaults = {
+          vimgrep_arguments = {
+            "rg",
+            "-L",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+          },
+          prompt_prefix = "   ",
+          selection_caret = "  ",
+          entry_prefix = "  ",
+          initial_mode = "insert",
+          selection_strategy = "reset",
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+              results_width = 0.8,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.9,
+            height = 0.8,
+            preview_cutoff = 120,
+          },
+          file_sorter = require("telescope.sorters").get_fuzzy_file,
+          file_ignore_patterns = { "node_modules", ".git" },
+          generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+          path_display = { "truncate" },
+          winblend = 0,
+          border = {},
+          borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+          color_devicons = true,
+          set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+          file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+          grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+          qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+          -- Developer configurations: Not meant for general override
+          buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+          mappings = {
+            n = { ["q"] = require("telescope.actions").close },
+            i = {
+              ["<C-k>"] = actions.move_selection_previous,                       -- move to prev result
+              ["<C-j>"] = actions.move_selection_next,                           -- move to next result
+              ["<S-Tab>"] = actions.move_selection_previous,                     -- move to prev result
+              ["<Tab>"] = actions.move_selection_next,                           -- move to next result
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+            }
+          },
+        },
+
+        extensions_list = { "themes", "terms" },
+        extensions = {},
+      }
+    )
     telescope.load_extension("fzf")
 
     local k = vim.keymap
